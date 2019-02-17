e4bda3c0c8e208febf97e4475757ce65d10adf71
Asaf Blum <minus199@gmail.com>
2019-02-17 21:09:34 +0200

    removed role mw from me controller

--- routes/me.js
+++ routes/me.js
@@ -2,16 +2,16 @@ const express = require("express");
 const router = express.Router();
 const ACL = require("../service/acl");
 
-router.use((req, res, next) => {
-  if (
-    req.currentUser.role === ACL.roles.SUPER_ADMIN ||
-    req.currentUser.role === ACL.roles.OWNER
-  ) {
-    return next();
-  }
+// router.use((req, res, next) => {
+//   if (
+//     req.currentUser.role === ACL.roles.SUPER_ADMIN ||
+//     req.currentUser.role === ACL.roles.OWNER
+//   ) {
+//     return next();
+//   }
 
-  next("Failed due to role mismatch");
-});
+//   next("Failed due to role mismatch");
+// });
 
 router.get("/profile", function(req, res, next) {
   res.json({ title: "Express" });
