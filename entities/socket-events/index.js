7e51a734aa77b2e50eb22c366bcf60e67d99cfd7
Asaf Blum <minus199@gmail.com>
2019-02-21 16:10:41 +0200

    export events history

--- entities/socket-events/index.js
+++ entities/socket-events/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     IncomingSocketEvent: require("./incoming-socket-event"),
     OutgoingSocketEvent: require("./outgoing-socket-event"),
-    SocketEvent: require("./socket-event")
+    SocketEvent: require("./socket-event"),
+    recentEvents: require("./events-history")
 }
